979cbe609c22e48aaa5ac937f1168283db39133c
ImpressGroup <ib-wd@impress.co.jp>
2019-01-11 11:42:54 +0800

    config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,28 @@
 module.exports = {
-  title: 'Netlify CMS + VuePress',
-  description: 'Netlify + VuePress',
+  title: '滑雪饭 Skier',
+  description: '大人孩子同乐乐，奉上最热的滑雪信息、玩转各大滑雪胜地、实战装备全攻略！ 北海道、长野等日本滑雪之旅的人气路线一网打尽~顺便泡泡温泉也不错!',
   themeConfig: {
     docsDir: 'docs',
-    repo: 'andreliem/vuepress-netlify-cms',
-    sidebar: [
-      '/',
-      '/welcome',
-      '/test'
-    ],
+    //repo: 'ImpressGroup/vuepressCMS',
+    search: false,
+    sidebar: false,
     nav: [
       {
-        text: 'Admin',
-        link: '/admin/#/',
+        text: '首页',
+        link: '/index.html',
+        
+      },
+      {
+        text: '滑雪旅游',
+        link: '/categories/travel',
+      },
+      {
+        text: '滑雪教程',
+        link: '#',
+      },
+      {
+        text: '滑雪用品',
+        link: '#',
       }
     ]
   }
